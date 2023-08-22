e66f1ef3bed10f374ce88a786b6185157c67046e
David Y <d.yang@pinkelephant.com>
2023-08-22 16:36:44 -0400

    append to the front and fix the tail updated to the newest node issue

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ class LinkedList:
             self.head = self.tail = new_node
             return
         
-        self.tail.prev = new_node
-        new_node.next = self.tail
-        self.head = self.tail = new_node
+        self.head.prev = new_node
+        new_node.next = self.head
+        self.head = new_node
 
     # delete node based on value
     def remove(self, data):
