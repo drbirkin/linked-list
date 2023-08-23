40d3036eb0734fa8f5e674eba422bd5f884f5416
david <davidliat98@gmail.com>
2023-08-23 00:20:30 -0400

    add find value and remove node from left to right

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ class LinkedList:
 
     def append_left(self, data):
         new_node = Node(data)
-        
+
         if not self.head:
             self.head = self.tail = new_node
             return
-        
+
         self.head.prev = new_node
         new_node.next = self.head
         self.head = new_node
-        
+
     def append_right(self, data):
         new_node = Node(data)
-        
+
         if not self.tail:
             self.head = self.tail = new_node
             return
@@ -32,28 +32,33 @@ class LinkedList:
         self.tail.next = new_node
         # overwrite the reference with current node
         self.tail = new_node
-        
+
 
     # delete node based on value
     def remove(self, data):
         current = self.head
         # print(current, self.head)
-        prev_node = None
         while current:
+            next_node = current.next
+            prev_node = current.prev
+
             if current.data == data:
-                tmp = current.next
-                current.next = None
-                print(tmp, current.next)
-                if prev_node:
-                    prev_node.next = tmp
+                current.next = current.prev = None
+
+                if next_node:
+                    next_node.prev = prev_node
                 else:
-                    self.head = tmp
+                    self.tail = prev_node
+
+                if prev_node:
+                    prev_node.next = next_node
+                else:
+                    self.head = next_node
                 # current is holding the ref of self.head but self.head is the node variable which needs to update as well, if removes head
-                current = tmp
+                current = next_node
                 # return
             else:
-                prev_node = current
-                current = current.next
+                current = next_node
 
     def display_forward(self):
         current = self.head
@@ -61,7 +66,7 @@ class LinkedList:
             print(current.data, end=" <-> ")
             current = current.next
         print("None")
-        
+
     def display_backward(self):
         current = self.tail
         while current:
@@ -82,7 +87,7 @@ llist.append_right(10)
 llist.append_right(30)
 llist.append_right(10)
 
-# llist.remove(20)
+llist.remove(10)
 
 # Displaying the linked list
 llist.display_forward()
